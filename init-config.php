976aba8681ae41a117c43c6a8af178d849f4785d
samkeen <skeen@mixpo.com>
2015-08-24 11:48:50 -0700

    added templates for the aws cloudwatch logs config files - init-config now creates to production files form the templates - cf-template mv's the files into place on the build maching and   completes the awslogs install fixes #6

--- init-config.php
+++ init-config.php
@@ -37,6 +37,10 @@ $cliDefinitions = [
     ]
 ];
 
+/**
+ * @param array $cliDefinitions
+ * @return string
+ */
 function getUsage($cliDefinitions)
 {
     $usage = "Usage:\n";
@@ -47,6 +51,12 @@ function getUsage($cliDefinitions)
     return $usage;
 }
 
+/**
+ * @param string $argName
+ * @param array $cliArgs
+ * @param array $cliDefinitions
+ * @return string
+ */
 function getCliArg($argName, $cliArgs, $cliDefinitions)
 {
     $argValue = getCliArgValue($cliArgs, "--{$argName}");
@@ -58,16 +68,36 @@ function getCliArg($argName, $cliArgs, $cliDefinitions)
     return $argValue;
 }
 
+/**
+ * @param array $cliDefinitions
+ * @param array $argv
+ * @param \Io\Samk\AmiBuilder\Utils\Config $config
+ */
 function processCli($cliDefinitions, $argv, $config)
 {
     foreach($cliDefinitions as $cliArgName => $cliArgDef) {
         list($sectionKey, $itemKey) = explode('.', $cliArgDef['configKeyPath']);
         $config->setSectionValue($sectionKey, $itemKey, getCliArg($cliArgName, $argv, $cliDefinitions));
     }
+}
 
+/**
+ * @param string $awsRegion
+ */
+function createCloudWatchLogsConfigs($awsRegion) {
+    $cloudWatchLogsConfDir = __DIR__ . '/config/cloudwatchLogs';
+    $awsCliConfContent = file_get_contents("{$cloudWatchLogsConfDir}/awscli.dist.conf");
+    $awsCliConfContent = str_replace('{{aws-region}}', $awsRegion,$awsCliConfContent);
+    file_put_contents("{$cloudWatchLogsConfDir}/awscli.conf", $awsCliConfContent);
+    // nothing to replace in this file
+    copy("{$cloudWatchLogsConfDir}/awslogs.dist.conf", "{$cloudWatchLogsConfDir}/awslogs.conf");
 }
 
 $logger = getAppLogger(getExecutionUuid());
 $config = getConfig(__DIR__ . '/config/config.dist.yml', $logger);
 processCli($cliDefinitions, $argv, $config);
 $config->dumpYamlTo(__DIR__ . "/config/config.yml");
+
+// write the AWS CloudWatch Logs configs to ./config/cloudwatchLogs
+// the deploy script will mv them to the proper place on the machine filesystem
+createCloudWatchLogsConfigs(getCliArg('awsRegion', $argv, $cliDefinitions));
